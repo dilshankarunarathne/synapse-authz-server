a15f442e4e134bfc7511bb75313d2ec4833bda98
dilshankarunarathne <maleeshasparrow@gmail.com>
2024-10-29 17:03:20 +0530

    changed server port

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.use(express.json());
 
 app.use('/auth', authController);
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(5000, () => console.log('Server started on port 5000'));
